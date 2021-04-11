2674253089217fa69899849a8981892923b34964
MuhammadTauseef <complexnumber@gmail.com>
2021-04-11 06:17:52 +0300

    Update __init__.py

--- gym/envs/__init__.py
+++ gym/envs/__init__.py
@@ -186,6 +186,14 @@ register(
     reward_threshold=8, # optimum = 8.46
     max_episode_steps=200,
 )
+# edited Tauseef start
+register(
+    id='Taxi-v4',
+    entry_point='gym.envs.toy_text:TaxiEnv2',
+    reward_threshold=8, # optimum = 8.46
+    max_episode_steps=200,
+)
+# edited Tauseef end
 
 register(
     id='GuessingGame-v0',
